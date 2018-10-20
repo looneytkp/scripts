ff44e0d603607dd3cc22f1581f44712a81bafdcd
looneytkp <sgm4kv@gmail.com>
2018-10-20 23:22:00 +0000

    .

---
+++ test.py
@@ -0,0 +1,22 @@
+#android-tools
+#git
+#zsh
+#youtube-dl
+#python-pip
+#beautifulsoup
+
+
+#chsh -s $(which zsh)
+#gnome-session-quit
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+from os import system as sys, path
+
+pacman = ['/usr/bin/git', '/usr/bin/zsh', 'usr/bin/youtube-dl', '/usr/bin/android-tools']
+
+sys(pacman)
+
+#git
+if not path.exist('/usr/bin/git'):
+    name = "looneytkp"
+    email = 'sgm4kv@gmail.com'
