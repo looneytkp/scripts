03a860e81bec2215055155bb095347e4789ddbc8
looneytkp <sgm4kv@gmail.com>
2019-01-02 09:06:31 +0000

    --

--- editor.sh
+++ editor.sh
@@ -1,43 +1,22 @@
 #!/bin/bash
+#v2.1.9
 set -e
-shopt -u nocasematch
-cd ~/Downloads
-format='.*(webrip|avi|flv|wmv|mov|mp4|mkv|3gp|webm|m4a|m4v|f4a|f4v|m4b|m4r|f4b).*</a>'
 connect(){
-	if grep -q "unable" .wget; then
-		grep -wE --color=never unable .wget;exit
-	fi
+	if grep -q "unable" .wget; then grep -wE --color=never unable .wget;exit 0;fi
 }
 add_on(){
-	if [ $func == 'header' ]; then
+	if [ "$func" == 'header' ]; then
 		{
-			if grep -qioE 's01' .ct;then echo '<h3>Season 1</h3>'
-			elif grep -qioE 's02' .ct;then echo '<h3>Season 2</h3>'
-			elif grep -qioE 's03' .ct;then echo '<h3>Season 3</h3>'
-			elif grep -qioE 's04' .ct;then echo '<h3>Season 4</h3>'
-			elif grep -qioE 's05' .ct;then echo '<h3>Season 5</h3>'
-			elif grep -qioE 's06' .ct;then echo '<h3>Season 6</h3>'
-			elif grep -qioE 's07' .ct;then echo '<h3>Season 7</h3>'
-			elif grep -qioE 's08' .ct;then echo '<h3>Season 8</h3>'
-			elif grep -qioE 's09' .ct;then echo '<h3>Season 9</h3>'
-			elif grep -qioE 's10' .ct;then echo '<h3>Season 10</h3>'
-			elif grep -qioE 's11' .ct;then echo '<h3>Season 11</h3>'
-			elif grep -qioE 's12' .ct;then echo '<h3>Season 12</h3>'
-			elif grep -qioE 's13' .ct;then echo '<h3>Season 13</h3>'
-			elif grep -qioE 's14' .ct;then echo '<h3>Season 14</h3>'
-			elif grep -qioE 's15' .ct;then echo '<h3>Season 15</h3>'
-			elif grep -qioE 's16' .ct;then echo '<h3>Season 16</h3>'
-			elif grep -qioE 's17' .ct;then echo '<h3>Season 17</h3>'
-			elif grep -qioE 's18' .ct;then echo '<h3>Season 18</h3>'
-			elif grep -qioE 's19' .ct;then echo '<h3>Season 19</h3>'
-			elif grep -qioE 's20' .ct;then echo '<h3>Season 20</h3>'
-			elif grep -qioE 's21' .ct;then echo '<h3>Season 21</h3>'
-			elif grep -qioE 's22' .ct;then echo '<h3>Season 22</h3>'
-			elif grep -qioE 's23' .ct;then echo '<h3>Season 23</h3>'
-			#optionally add more headers here
+			if grep -qioE "s[0-9][0-9]" .ct; then
+				_a=$(grep -iwE "s[0-9][0-9]" .ct|
+				sed -e "s/[S-s]/<h3>Season /" -e 's/$/<\/h3>/')
+				_b=$(grep -iwE "s[0-9][0-9]" .ct|
+				sed -e 's/[S-s]/<h3>Season /' -e 's/[0-9]//' -e 's/$/<\/h3>/')
+				_c=$(grep -iwE "s[0-9][0-9]" .ct|sed -e 's/[S-s]//' -e 's/[0-9]$//')
+				if [ "$_c" -ge 1 ]; then echo "$_a"; else echo "$_b"; fi
 			fi
 		} >> .out
-	elif [ $func == 'start' ]; then
+	elif [ "$func" == 'start' ]; then
 		wget -k "$url" -o .wget -O .ct||connect
 		if grep -iqE '.*(&amp|&darr).*' .ct; then
 			grep -vE '.*(amp|darr).*' .ct > .ct2;mv .ct2 .ct
@@ -48,21 +27,19 @@ add_on(){
 				grep -iwE ".*trailer.*</a>" .ct|sed 's:.*<a:<a:';echo
 			fi
 		} >> .out
-	elif [ $func == 'proc' ]; then
-		####################testing
+	elif [ "$func" == 'proc' ]; then
 		c=1
 		while true; do
-			if [ $c -gt 9 ]; then pattern="(e$c|e$c)"
-			else pattern="(e$c|e0$c)"
+			if [ $c -gt 9 ]; then d="(e$c|e$c)"
+			else d="(e$c|e0$c)"
 			fi
-			if grep -qiwE ".*$pattern" .ct; then
-				grep -iwE ".*$pattern" .ct >> .ct2
+			if grep -qiwE ".*$d" .ct; then
+				grep -iwE ".*$d" .ct >> .ct2
 			else
 				if [ -e .ct2 ]; then mv .ct2 .ct;fi;break
 			fi
 			c=$((c+1))
 		done
-		####################testing
 		if ! grep -qE "(480p|720p|1080p)" .ct; then
 				grep -iowE "$format" .ct|sed 's:.*<a:<a:' >> .out
 		else
@@ -127,7 +104,7 @@ sort(){
 			fi
 			size=$(wc -l<.out)
 			if [ "$url" == '' ]; then
-				if [ $size -le 1 ]; then
+				if [ "$size" -le 1 ]; then
 					echo "aborted";rm .out .wget 2> /dev/null;return
 				else
 					echo '[/vc_column_text][/vc_column][/vc_row]' >> .out
@@ -139,8 +116,7 @@ sort(){
 				echo > /dev/null
 			else
 				func='start';add_on;func='header';add_on
-				func='proc';add_on
-				rm .ct
+				func='proc';add_on;rm .ct
 			fi
 			b=$((b+1))
 		else
@@ -149,6 +125,8 @@ sort(){
 	done
 	'sort'
 }
+shopt -u nocasematch
+format='.*(webrip|avi|flv|wmv|mov|mp4|mkv|3gp|webm|m4a|m4v|f4a|f4v|m4b|m4r|f4b).*</a>'
 if [ -e .out ]; then rm .out; fi
 if [ -e .ct ]; then rm .ct*; fi
 if [ -e .wget ]; then rm .wget; fi
@@ -157,21 +135,32 @@ case $1 in
 	480p|480)	pixel="480p";edit;;
 	720p|720)	pixel="720p";edit;;
 	1080p|1080)	pixel="1080p";edit;;
-	sort)	'sort';;
-	install)	if [[ ! -e /usr/bin/editor ]]; then
-					sudo cp editor.sh /usr/bin/editor
-					echo -e "installed editor."
-				else
-					a=$(md5sum editor.sh|sed 's: editor.sh::')
-					b=$(md5sum /usr/bin/editor|
-					sed 's: /usr/bin/editor::')
-					if [[ "$a" != "$b" ]]; then
-						sudo cp -u editor.sh /usr/bin/editor
-						echo -e "updated editor."
-					else
-						echo "redundant update, no changes found."
-					fi
-				fi;;
-	help)	echo -e "\\nusage: editor [options]\\noptions:\\n    480/480p	-	grab 480p URLs.\\n    720/720p	-	grab 720p URLs.\\n    1080/1080p	-	grab 1080p URLs.\\n    sort	-	grab & parse all URLs in order.\\n    install	-	install editor.\\n    help	-	show help.\\n";;
-	*) echo "invalid argument: $1";;
+	-s|--sort)	'sort';;
+	-i|--install)
+		_dir=$(echo "$PWD")
+		if [[ ! -e /usr/bin/editor ]]; then
+			sudo cp "$_dir"/editor.sh /usr/bin/editor && sudo chmod 777 /usr/bin/editor
+			echo -e "installed editor."
+		else
+			if [ "$0" == '/usr/bin/editor' ]; then
+				echo 'editor is already installed.'
+				exit 0
+			fi
+			a=$(md5sum "$_dir"/editor.sh|sed 's:  /.*editor.sh::')
+			b=$(md5sum /usr/bin/editor|sed 's:  /usr/bin/editor::')
+			if [[ "$a" != "$b" ]]; then
+				sudo cp -u "$_dir"/editor.sh /usr/bin/editor;sudo chmod 777 /usr/bin/editor
+				echo -e "updated editor."
+			else
+				echo "editor is up-to-date."
+			fi
+		fi;;
+	-u|--uninstall)
+		if [ -e /usr/bin/editor ]; then sudo rm /usr/bin/editor
+			echo 'uninstalled editor'
+		else
+			echo 'editor is not installed'
+		fi;;
+	-h|--help)	echo -e "\\nusage: editor [...arguments]\\narguments:\\n   480, 480p.\\n   720, 720p.\\n   1080, 1080p.\\n   -s, --sort.\\n   -i, --install.\\n   -u, --uninstall.\\n   -h, --help.\\n";;
+	*) echo -e "invalid argument: $1\\ntry 'editor -h for more information.";;
 esac
