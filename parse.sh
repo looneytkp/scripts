ffbb5a12830acec2008020e1ce82e4a8d8814723
looneytkp <sgm4kv@gmail.com>
2019-01-08 05:47:02 +0000

    --

--- parse.sh
+++ parse.sh
@@ -1,45 +1,62 @@
 #!/bin/bash
-#v3.8.4
+#v4.2.7
 #looneytkp
 cleanup(){
 	if [ -e .out ]; then rm .out*; fi
-	if [ -e .ct ]; then rm .ct*;fi
+	if [ -e .ct ]; then rm .ct*; fi
 	if [ -e .wget ]; then rm .wget; fi
 	if [ -d $_dir ]; then rm -rf $_dir; fi
 }
+
 sig_abort(){
 	cleanup
 	echo -e "\\naborted.\\n"
 	exit 0
 }
+
 abort(){
-	#cleanup
-	if [ "$nd2" != '' ]; then
-		if [ $size -ge 1 ]; then
-			echo -e ":: faulty: $url.\\n"
+	cleanup
+	if [ $esc == yes ]; then
+		if [ "$size" -lt 1 ]; then
+			echo -e ":: copied to clipboard.\\n"
 		else
-			if [ ! -e $out ]; then
-				truncate -s 0 $out;size=$(wc -l<$out)
-				echo -e ":: faulty: $url.\\n"
-			else
-				xclip -sel clip < $out
-				echo -e ":: copied to clipboard.\\n"
-			fi
+			echo -e ":: let's find out'.\\n"
+		fi
+	elif [ $esc == no ]; then
+		if [ $size -lt 1 ]; then
+			echo -e "aborted.\\n"
+		else
+			echo -e ":: failed.\\n"
 		fi
 	else
 		echo -e "aborted.\\n"
 	fi
-	exit 0
+	exit 0		
 }
+
 connect(){
 	if [ $? == 4 ]; then
 		grep -wE --color=never unable .wget|sed 's:wget:error:'
-		echo -e "check internet connection!\\n";abort
+		if [ "$nd2" != '' ]; then
+			echo -e ":: check internet connection!\\n"
+		else
+			echo -e "check internet connection!\\n"
+		fi
+		abort
 	fi
 }
+
 parser(){
-if [ "$cur" == fl ]; then ct=$file;fi
-f=(480p 720p 1080p)
+	if [ "$nd2" != '' ]||[ $n -lt 1 ]&&[ "$cur" == fl ]; then
+			ct=$file
+			if ! grep -oq "Season $cmpr" $ct; then echo >> $out;fi
+			grep -o '.*Season.*' $ct|head -1 >> $out
+	fi
+	if ! grep -qE "(480p|720p|1080p)" $ct; then
+		echo >> $out
+		grep -iowE "$format" $ct|sed 's:.*<a:<a:' >> $out
+	else
+		f=(480p 720p 1080p)
 			for f in "${f[@]}"; do
 				if grep -qioE ".*$f.*</a>" $ct; then
 					grep -iwE ".*$f.*" $ct|
@@ -81,8 +98,11 @@ f=(480p 720p 1080p)
 					fi
 				fi
 			done
-			if [ "$cur" != fl ]; then if [ -e $ct3 ]; then rm $ct2 $ct3;fi;fi
+	fi
+	if [ -e $ct2 ]; then rm $ct2;fi
+	if [ -e $ct3 ]; then rm $ct3;fi
 }
+
 add_on(){
 	if [ "$func" == 'header' ]; then
 		wget -k "$url" -o .wget -O $ct||connect
@@ -120,17 +140,12 @@ add_on(){
 			fi
 			c=$((c+1))
 		done
-		if ! grep -qE "(480p|720p|1080p)" $ct; then
-				grep -iowE "$format" $ct|sed 's:.*<a:<a:' >> $out
-		else
-			cur=lf
-			parser
-			if [ -e $ct2 ]; then rm $ct2;fi
-		fi
+		parser
 	fi
 }
+
 edit(){
-	esc=no;esc=$(echo $esc);nd2='';nd2=$(echo "$nd2")
+	esc=no;esc=$(echo $esc)
 	if ! grep -q "#editor=no-talk" ~/.bashrc; then
 		printf %b "            [hit enter if input is null to abort]\\r"
 	fi
@@ -167,12 +182,12 @@ edit(){
 			grep -iowE ".*$pixel.*</a>" $ct|sed 's:.*<a:<a:' >> $out
 	esac
 	xclip -sel clip < $out
-	echo -e "copied to clipboard."
+	echo -e "\\ncopied to clipboard."
 	rm $out $ct .wget
 	edit
 }
+
 sort(){
-#here
 	compare(){
 		size=$(wc -l<$out)
 		if [ $size -le 1 ]; then
@@ -180,15 +195,29 @@ sort(){
 			b=$((b+0))
 			status=1
 		else
-			cmpr=$(grep -iwoE "s$srt" $out|head -1|sed 's:[S-s]::')
-			if [ "$cmpr" == "$srt" ]; then
-				cat $out >> "$_dir/s$srt"
+			if [ "$nd2" != '' ]; then
+				cmpr=$(grep -o "Season [0-9][0-9]" $out|sed 's:Season ::')
+				if grep -oq "Season $cmpr" $out && grep -oqs "Season $cmpr" $_dir/s$cmpr;then
+					sed -i "/Season $cmpr/d" $out
+				else
+					true
+				fi
+				cat $out >> "$_dir/s$cmpr";unset cmpr
 				truncate -s 0 $out
 				b=$((b+0))
 				empty=no;status=0
 			else
-				b=$((b-1))
-				status=1
+				cmpr=$(grep -iwoE "s$srt" $out|head -1|sed 's:[S-s]::')
+				if [ "$cmpr" == "$srt" ]; then
+					cat $out >> "$_dir/s$srt"
+					truncate -s 0 $out
+					b=$((b+0))
+					empty=no;status=0
+				else
+					truncate -s 0 $out
+					b=$((b+0))
+					status=1
+				fi
 			fi
 		fi
 	}
@@ -208,12 +237,18 @@ sort(){
 			else
 				header=n;func='header';add_on
 				func='proc';add_on
-				cmpr=$(grep -iwoE "s01" $out|head -1|sed 's:[S-s]::')
+				cmpr=$(grep -iwoE "s$srt" $out|sed 's:[S-s]::'|head -1)
 				if [ "$cmpr" != "$srt" ]; then
 					echo -e ":: error:'s$srt' not found in URL."
-					truncate -s 0 $out;empty="yes";status=2
+					truncate -s 0 $out;unset cmpr;empty="yes";status=2
 				else
-					cat $out >> $out2
+					cmpr2=$(grep -oE "(480p|720p|1080p)" $out|head -1)
+					cmpr3=$(grep -oEs "(480p|720p|1080p)" $out2|head -1)
+					if [ "$cmpr2" == "$cmpr3" ]; then
+						echo ":: duplicate: $cmpr2.";rm $out
+					else
+						cat $out >> $out2;rm $out;unset cmpr
+					fi
 				fi
 			fi
 		fi
@@ -228,14 +263,14 @@ sort(){
 		fi
 	fi
 	a=0;b=1;d=0
-	touch $out
+	#touch $out
 	esc=$(echo $esc)
 	while true;do
 		talk(){
 			if ! grep -q "#editor=no-talk" ~/.bashrc; then
-				text=("[flags:append mod skip return delete exit]" "[append:adds to the end of existing texts]" "[skip:hops to the next tag.]" "[exit:aborts the script]" "[return:hops to previous tag]" "[tip:pass '--no-talk' to disable ghost-texts]"  "[modify:edits the current tag file]" "[delete: erases current tag]" "[tip:letters are also flags. eg. s = skip]" "[letter flags: a, m, s, r, d, e]" "[tip:hit enter to abort if input is null]" "[info:input URLs that match with tags]" "[info:tags are s01 s02 s03 ... etc]" "[tip:hit enter to clip URL if input is valid]" "[tip:sort supports positional parameters]" "[tip:pass '--talk' to enable ghost-texts]")
+				text=("[flags:append mod skip return delete exit]" "[append:adds to the end of existing texts]" "[skip:hops to the next tag.]" "[exit:aborts the script]" "[return:hops to previous tag]" "[info:pass '--no-talk' to disable ghost-texts]"  "[modify:edits the current tag file]" "[delete: erases current tag]" "[info:letters are also flags. eg. s = skip]" "[letter flags: a, m, s, r, d, e]" "[info:bad links aborts positional parsing]" "[info:hit enter to abort if input is null]" "[info:input URLs that match with tags]" "[info:tags are s01 s02 s03 ... etc]" "[info:hit enter to clip URL if input is valid]" "[info:sort supports positional parameters]" "[info:pass '--talk' to enable ghost-texts]")
 				ezis=${#text[@]};xeden=$((RANDOM % $ezis))
-				printf %b "              ${text[$xeden]}\\r"
+				printf %b "                  ${text[$xeden]}\\r"
 			fi
 		}
 		if [ $b -ge 10 ]; then
@@ -246,7 +281,6 @@ sort(){
 			else
 				d=0;srt="$d$b"
 			fi
-		
 		fi
 		if [ $a != 1 ]; then
 			if [ $n -lt 1 ]; then
@@ -262,52 +296,35 @@ sort(){
 					if [ -z "$(ls -A $_dir)" ]; then
 						abort
 					else
-						
-						if [ $size == 2 ]; then #&& [ "$(wc -l<$_dir/s$srt)" -le 2 ]; then
-							abort
-						#elif [ $size -lt 2 ] && [ "$(wc -l<$_dir/s$srt)" -gt 2 ]; then
-						#	abort
+						echo -e "[vc_row][vc_column column_width_percent=\"100\" align_horizontal=\"align_center\" overlay_alpha=\"50\" gutter_size=\"3\" medium_width=\"0\" mobile_width=\"0\" shift_x=\"0\" shift_y=\"0\" shift_y_down=\"0\" z_index=\"0\" width=\"1/1\"][vc_column_text]\\n" > $out
+						if [ "$nd2" != '' ]||[ $n -lt 1 ];then
+							cur=fl;cur=$(echo $cur)
+							for file in $_dir/*;do
+								parser
+							done
 						else
-							echo '[vc_row][vc_column column_width_percent="100" align_horizontal="align_center" overlay_alpha="50" gutter_size="3" medium_width="0" mobile_width="0" shift_x="0" shift_y="0" shift_y_down="0" z_index="0" width="1/1"][vc_column_text]' > $out
-							if [ $n -ge 1 ] && [ $esc == no ]; then
-								for file in $_dir/*;do
-									cat $file >> $out
-								done
-							else
-								cur=fl;cur=$(echo $cur)
-								for file in $_dir/*;do
-									grep -qio '.*Season.*' $file >> $out
-									parser
-								done
-							fi
-							echo -e "\\n[/vc_column_text][/vc_column][/vc_row]" >> $out
-							#xclip -sel clip < $out
-							if [ $esc == yes ]; then 
-								if [ $n -le 1 ]; then 
-									rm $out;return
-								else
-									xclip -sel clip < $out
-									echo "copied to clipboard."
-									rm $out .wget;break
-									b=$((b+1))
-								fi
-							else
-								xclip -sel clip < $out
-								echo "copied to clipboard."
-								rm $out .wget;break
-								b=$((b+1))
-							fi
+							for file in $_dir/*;do
+								cat $file >> $out
+							done
+						fi
+						echo -e "\\n[/vc_column_text][/vc_column][/vc_row]" >> $out
+						xclip -sel clip < $out
+						if [ $esc == yes ]; then echo ":: done."
+							rm $out;return
+						else echo "copied to clipboard."
+							rm $out .wget;break
 						fi
 					fi;;
 				skip|s)
 					echo > /dev/null;b=$((b+1));;
 				append|a)
-					pixel=":: url";echo ":: appending texts.";add
+					pixel=":: url";echo ":: appending links to s$srt.";add
 					status=$(echo "$status");empty=$(echo "$empty")
 					if [ "$status" == 2 ] && [ "$empty" == "yes" ]; then
 						add
 					elif [ "$status" == 1 ] && [ "$empty" == "yes" ]; then
 						echo -e ":: exited.\\n"
+						#if [ ! -e $_dir/s$srt ]; then b=$((b-1));fi
 					elif [ "$status" == 1 ] && [ "$empty" == "" ]; then
 						echo -e ":: error: s$srt links not found, check URL."
 						add
@@ -315,8 +332,7 @@ sort(){
 						echo -e ":: done with s$srt.\\n";b=$((b+1))
 					fi;;
 				return|r)
-						b=$((b-1))
-					;;
+						b=$((b-1));;
 				delete|d)
 					if [ -e "$_dir/s$srt" ]; then rm "$_dir/s$srt";echo "s$srt deleted.";fi
 					b=$((b-0));;
@@ -324,42 +340,40 @@ sort(){
 					echo ":: not yet functional.";b=$((b-0));;
 				exit|e) abort;;
 				*)
-					if [ $n -lt 1 ]; then
-						for url in "${url[@]}";do
-							lru=$(echo "$url"|grep "http"|| echo false)
-							if [ $lru == false ]; then
-								echo "error: $url: not a URL/flag.";b=$((b+0))
-								if [ -e $_dir/s$srt ]; then rm "$_dir/s$srt";fi;break
-							else
-								header=y;func='header';add_on
-								func='proc';add_on
-								compare
-								if [ $status == 1 ]; then
-									echo "error: $url.";b=$((b+0))
-									#rm $ct #$ct2 $ct3
-									#cleanup;mkdir $_dir
-									truncate -s 2 $out;
-									break
-								fi
-							fi
-						done
-						b=$((b+1))
-						if [ $b -gt 1 ]; then b=$((b-0));fi # else b=$((b-1));fi
-					else
+					link(){
 						lru=$(echo "$url"|grep "http"|| echo false)
 						if [ $lru == false ]; then
-							echo ":: error: $url: not a URL/flag.";b=$((b+0))
-							if [ "$nd2" != "" ]; then abort;fi
+							if [ "$nd2" != "" ]; then
+								echo ":: error: $url: not a URL or flag."
+								echo > $out;size=$(wc -l<$out);abort
+							else
+								echo "error: $url: not a URL or flag.";b=$((b+0))
+							fi
 						else
 							header=y;func='header';add_on
 							func='proc';add_on;rm $ct
 							compare
-							if [ "$nd2" != "" ]; then b=$((b+1));return;fi
+							if [ "$nd2" != "" ]; then return;fi
 							if [ $status == 1 ]; then
-								echo ":: error: s$srt not found.";b=$((b-0))
+								echo "error: $url.";b=$((b-1))
+								if [ -e "$_dir/s$srt" ]; then rm "$_dir/s$srt";fi
+								error=yes;error=$(echo $error)
 							fi
 							b=$((b+1))
 						fi
+					}
+					if [ $n -lt 1 ]; then
+						for url in "${url[@]}";do
+							link
+							if [ "$error" == yes ]; then unset error;break;fi
+						done
+						arr=$(echo "${#url[@]}")
+						if [ $arr -gt 1 ]; then
+							b=$((b-arr+1))
+						fi
+					else
+						link
+						if [ "$nd2" != "" ]; then return;fi
 					fi;;
 			esac
 		else
@@ -368,32 +382,33 @@ sort(){
 	done
 	'sort'
 }
-set -xe
+name="parse"
+set -e
 trap sig_abort SIGINT
-#cleanup
 if [ ! -e /usr/bin/xclip ]; then
 	echo -e "install xclip: 'sudo <your-package-manager> xclip'."
 	exit 0
 fi
 shopt -u nocasematch
-out=.out
-out2=.out2
-ct=.ct
-ct2=.ct2
-ct3=.ct3
 format='.*(webrip|avi|flv|wmv|mov|mp4|mkv|3gp|webm|m4a|m4v|f4a|f4v|m4b|m4r|f4b).*</a>'
+out=.out;out2=.out2;ct=.ct;ct2=.ct2;ct3=.ct3
+#if [ -e $out ]; then rm .out*; fi
+#if [ -e $ct ]; then rm .ct*; fi
+#if [ -e .wget ]; then rm .wget; fi
+touch $out
+size=$(wc -l<$out)
 case $1 in
 	"")	pixel='url';echo;edit;;
 	480p|480)	pixel="480p";echo;edit;;
 	720p|720)	pixel="720p";echo;edit;;
 	1080p|1080)	pixel="1080p";echo;edit;;
-	--talk)
-		if grep -q "#editor=no-talk" ~/.bashrc; then
-			sed -i "/#editor=no-talk/d" ~/.bashrc
+	-t|--talk)
+		if grep -q "#$name=no-talk" ~/.bashrc; then
+			sed -i "/#$name=no-talk/d" ~/.bashrc
 		fi;;
-	--no-talk)
-		if ! grep -q "#editor=no-talk" ~/.bashrc; then
-			echo "#editor=no-talk" >> ~/.bashrc
+	-n|--no-talk)
+		if ! grep -q "#$name=no-talk" ~/.bashrc; then
+			echo "#$name=no-talk" >> ~/.bashrc
 		fi;;
 	-s|--sort)
 		if [ "$2" != '' ]; then
@@ -402,14 +417,16 @@ case $1 in
 			echo -e "\\n:: auto sorting $n URL(s)."
 			for position;do
 				for i in "${fc[@]}"; do
-					if [ $2 == $i ]; then echo -e ":: error: $i: not a URL or flag.";abort;fi
+					if [ "$position" == $i ]; then
+						echo -e ":: error: $i: not a URL or flag."
+						echo > $out;size=$(wc -l<$out);abort
+					fi
 				done
 				if [ "$position" == -s ]||[ "$position" == '--sort' ]; then
 					continue
 				else
 					n=$((n+1))
 					'sort' "$position"
-					status=$(echo "$status");empty=$(echo "$empty")
 					if [ "$status" == 1 ] && [ "$empty" == "yes" ]; then
 						echo ":: faulty URL: $url."
 						abort
@@ -423,35 +440,35 @@ case $1 in
 		fi;;
 	-i|--install)
 		_script=$(echo "$0")
-		if [[ ! -e /usr/bin/editor ]]; then
-			sudo cp "$_script" /usr/bin/editor && sudo chmod 777 /usr/bin/editor
-			echo -e "installed editor."
+		if [[ ! -e /usr/bin/$name ]]; then
+			sudo cp "$_script" /usr/bin/$name && sudo chmod 777 /usr/bin/$name
+			echo -e "installed $name."
 		else
-			if [ "$0" == '/usr/bin/editor' ]; then
-				echo 'editor is already installed.'
+			if [ "$0" == "/usr/bin/$name" ]; then
+				echo -e "\\n$name: already installed.\\n"
 				exit 0
 			fi
-			a=$(md5sum "$_script"|sed 's:  .*editor.sh::')
-			b=$(md5sum /usr/bin/editor|sed 's:  /usr/bin/editor::')
+			a=$(md5sum "$_script"|sed "s:  .*$name.sh::")
+			b=$(md5sum /usr/bin/$name|sed "s:  /usr/bin/$name::")
 			if [[ "$a" != "$b" ]]; then
-				sudo cp -u "$_script" /usr/bin/editor;sudo chmod 777 /usr/bin/editor
-				echo -e "updated editor."
+				sudo cp -u "$_script" /usr/bin/$name;sudo chmod 777 /usr/bin/$name
+				echo -e "\\n$name: updated.\\n"
 			else
-				echo "editor is up-to-date."
+				echo -e "\\n$name: up-to-date.\\n"
 			fi
 		fi
-		if grep -q "#editor=no-talk" ~/.bashrc; then
-			sed -i "/#editor=no-talk/d" ~/.bashrc
+		if grep -q "#$name=no-talk" ~/.bashrc; then
+			sed -i "/#$name=no-talk/d" ~/.bashrc
 		fi;;
 	-u|--uninstall)
-		if grep -q "#editor=no-talk" ~/.bashrc; then
-			sed -i "/#editor=no-talk/d" ~/.bashrc
+		if grep -q "#$name=no-talk" ~/.bashrc; then
+			sed -i "/#$name=no-talk/d" ~/.bashrc
 		fi
 		if [ -e /usr/bin/editor ]; then sudo rm /usr/bin/editor
-			echo 'uninstalled editor'
+			echo "uninstalled $name."
 		else
-			echo 'editor is not installed'
+			echo "$name is not installed"
 		fi;;
-	-h|--help)	echo -e "\\na simple html editor.\\nusage: editor [...arguments]\\narguments:\\n   480(p), 720(p), 1080(p).\\n   -s, --sort -{append, modify, delete, skip, return, exit}\\n   -i, --install.\\n   -u, --uninstall.\\n   -h, --help.\\n";;
-	*) echo -e "invalid flag: $1\\ntry 'editor -h for more information.";;
+	-h|--help)	echo -e "\\na simple html parser.\\nusage: editor [...flags]\\nflags:\\n   480(p)             -   parse 480p text.\\n   720(p)             -   parse 720p texts.\\n   1080(p)            -   parse 1080p text.\\n   -s, --sort         -   parse all text URL.\\n   -t, --talk         -   enable ghost-text.\\n   -n, --no-talk      -   silence ghost-text.\\n   -i,  --install     -   install parser.\\n   -u, --uninstall    -   uninstall parser.\\n   -h, --help         -   show help.\\n";;
+	*) echo -e "\\ninvalid argument: $1\\ntry '$name -h' for more information.\\n";;
 esac
